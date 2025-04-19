75211c621dfad8f699f4b0ca7950f34f1e7692cd
smallbraingames <smallbraindev@gmail.com>
2025-04-18 22:10:36 -0700

    feat(contracts): order tests

--- contracts/test/Sanity.t.sol
+++ contracts/test/Sanity.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.29;
 
 import {Test, console} from "forge-std/Test.sol";
 import {MaybePay} from "../src/MaybePay.sol";
