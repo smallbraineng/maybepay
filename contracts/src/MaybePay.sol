3be099e31c33342bebbc4c389e89ab71f81031ec
smallbraingames <smallbraindev@gmail.com>
2025-06-09 18:09:37 -0700

    feat(contracts): bump solidity

--- contracts/src/MaybePay.sol
+++ contracts/src/MaybePay.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.29;
+pragma solidity >=0.8.30;
 
 import {Owned} from "solmate/auth/Owned.sol";
 
--- contracts/test/Sanity.t.sol
+++ contracts/test/Sanity.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.29;
+pragma solidity >=0.8.30;
 
 import {Test, console} from "forge-std/Test.sol";
 import {MaybePay} from "../src/MaybePay.sol";
